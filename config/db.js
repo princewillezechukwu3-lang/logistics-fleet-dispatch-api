cc7bf615c46fc014837848cd579e921213229a73
Ezechukwu Princewill <princewillezechukwu3@gmail.com>
2026-06-27 12:28:48 +0100

    Configure: Implement dynamic environment staging and adaptive database connection pool

--- config/db.js
+++ config/db.js
@@ -1,8 +1,14 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: false
+const dbConnectionString = process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : process.env.LOCAL_DATABASE_URL
+const sslSetting = process.env.NODE_ENV === 'production' ? {rejectUnauthorized: false} : false
+
+const pool = new Pool ({
+    connectionString: dbConnectionString,
+    ssl: sslSetting
 })
+
+console.log(`Database Engine initialized in [${process.env.NODE_ENV || 'development'}] mode.`);
 
 module.exports = pool
